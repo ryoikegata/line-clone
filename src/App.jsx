eed627f88847e44f724f3a920cc2cf2f966485be
Ryo ikegata <ryo.ike.6153@gmail.com>
2023-10-19 13:58:56 +0900

    Add:react環境構築

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './index.css';
+
+function App() {
+
+  return (
+    <>
+      <div className='text-red-600'>
+        Hello, world!
+      </div>
+    </>
+  )
+}
+
+export default App;
